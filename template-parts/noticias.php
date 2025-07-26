cde46f7348b566962aa01b1765b5449782242ff2
Pedro <pedro.fialho000@gmail.com>
2025-07-26 20:26:12 -0300

    feat(posts): Adiciona seção de notícias com conteúdo dinâmico via GNews API

---
+++ template-parts/noticias.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WebForce Solutions</title>
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/noticias.css">
+</head>
+<body>
+    <main>
+        <section id="noticias">
+            <h1>Últimas notícias de Tecnologia</h1>
+            <ul id="lista-noticias"></ul>
+        </section>
+    </main>
+</body>
+</html> 
